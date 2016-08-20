e6c6f8fda301cd67f6c3c4cd823db5a40df330ce
Federico Teotini <templarblack.fed03@gmail.com>
2016-08-20 19:12:25 +0200

    Conditionally loading pouchdb-authentication

--- index.js
+++ index.js
@@ -3,8 +3,19 @@
 
 module.exports = {
   name: 'ember-pouch',
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    this.emberPouchOptions = this.project.config().emberPouch || {};
+  },
   included(app) {
     app.import(app.bowerDirectory + '/pouchdb/dist/pouchdb.min.js');
+    app.import(app.bowerDirectory + '/relational-pouch/dist/pouchdb.relational-pouch.min.js');
+
+    if (this.emberPouchOptions.enableAuth === true) {
+      app.import(app.bowerDirectory + '/pouchdb-authentication/dist/pouchdb.authentication.min.js');
+    }
+
     app.import('vendor/shims/pouchdb.js', {
       exports: { 'pouchdb': ['default'] }
     });
