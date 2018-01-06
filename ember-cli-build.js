c6ee7d5b274abd23e01ab2953634ab559b3906c5
Federico Teotini <templarblack.fed03@gmail.com>
2018-01-06 11:30:14 +0100

    removed bower

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
-'use strict';
+"use strict";
 
-const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const EmberAddon = require("ember-cli/lib/broccoli/ember-addon");
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
@@ -14,9 +14,5 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  app.import('bower_components/phantomjs-polyfill-object-assign/object-assign-polyfill.js', {
-    type: 'test'
-  });
-
   return app.toTree();
 };
