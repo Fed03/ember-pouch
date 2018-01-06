f53dc2ff512f6bbf2b16ee4b0c988f8189e6d77a
Federico Teotini <templarblack.fed03@gmail.com>
2018-01-06 12:13:45 +0100

    Attempt to switch to pouchdb-browser

--- index.js
+++ index.js
@@ -1,50 +1,73 @@
 /* eslint-env node */
-'use strict';
+"use strict";
 
-var path = require('path');
-var stew = require('broccoli-stew');
+var path = require("path");
+var stew = require("broccoli-stew");
+const Funnel = require("broccoli-funnel");
+const Rollup = require("broccoli-rollup");
 
 module.exports = {
-  name: 'ember-pouch',
+  name: "ember-pouch",
 
   init: function() {
     this._super.init && this._super.init.apply(this, arguments);
 
     var bowerDeps = this.project.bowerDependencies();
 
-    if (bowerDeps['pouchdb']) {this.ui.writeWarnLine('Please remove `pouchdb` from `bower.json`. As of ember-pouch 4.2.0, only the NPM package is needed.');}
-    if (bowerDeps['relational-pouch']) {this.ui.writeWarnLine('Please remove `relational-pouch` from `bower.json`. As of ember-pouch 4.2.0, only the NPM package is needed.');}
-    if (bowerDeps['pouchdb-find']) {this.ui.writeWarnLine('Please remove `pouchdb-find` from `bower.json`. As of ember-pouch 4.2.0, only the NPM package is needed.');}
+    if (bowerDeps["pouchdb"]) {
+      this.ui.writeWarnLine("Please remove `pouchdb` from `bower.json`. As of ember-pouch 4.2.0, only the NPM package is needed.");
+    }
+    if (bowerDeps["relational-pouch"]) {
+      this.ui.writeWarnLine("Please remove `relational-pouch` from `bower.json`. As of ember-pouch 4.2.0, only the NPM package is needed.");
+    }
+    if (bowerDeps["pouchdb-find"]) {
+      this.ui.writeWarnLine("Please remove `pouchdb-find` from `bower.json`. As of ember-pouch 4.2.0, only the NPM package is needed.");
+    }
   },
 
-  treeForVendor: function() {
-    var pouchdb = stew.find(path.join(path.dirname(require.resolve('pouchdb')), '..', 'dist'), {
-      destDir: 'pouchdb',
-      files: ['pouchdb.js']
-    });
+  // treeForVendor: function() {
+  //   var pouchdb = stew.find(path.join(path.dirname(require.resolve('pouchdb')), '..', 'dist'), {
+  //     destDir: 'pouchdb',
+  //     files: ['pouchdb.js']
+  //   });
+  //
+  //   var relationalPouch = stew.find(path.join(path.dirname(require.resolve('relational-pouch')), '..', 'dist'), {
+  //     destDir: 'pouchdb',
+  //     files: ['pouchdb.relational-pouch.js']
+  //   });
+  //
+  //   var shims = stew.find(__dirname + '/vendor/pouchdb', {
+  //     destDir: 'pouchdb',
+  //     files: ['shims.js']
+  //   });
+  //
+  //   return stew.find([
+  //     pouchdb,
+  //     relationalPouch,
+  //     shims
+  //   ]);
+  // },
 
-    var relationalPouch = stew.find(path.join(path.dirname(require.resolve('relational-pouch')), '..', 'dist'), {
-      destDir: 'pouchdb',
-      files: ['pouchdb.relational-pouch.js']
+  treeForVendor() {
+    var pouchdb = new Funnel(path.dirname(require.resolve("pouchdb-browser")));
+    return new Rollup(pouchdb, {
+      rollup: {
+        input: "./index.js",
+        output: {
+          file: "pouchdb.js",
+          format: "iife"
+        }
+      }
     });
-
-    var shims = stew.find(__dirname + '/vendor/pouchdb', {
-      destDir: 'pouchdb',
-      files: ['shims.js']
-    });
-
-    return stew.find([
-      pouchdb,
-      relationalPouch,
-      shims
-    ]);
   },
 
   included(app) {
-    app.import('vendor/pouchdb/pouchdb.js');
-    app.import('vendor/pouchdb/pouchdb.relational-pouch.js');
-    app.import('vendor/pouchdb/shims.js', {
-      exports: { 'pouchdb': [ 'default' ]}
-    });
+    app.import("vendor/pouchdb.js");
+    app.import("node_modules/relational-pouch/dist/pouchdb.relational-pouch.js");
+    // app.import('vendor/pouchdb/pouchdb.js');
+    // app.import('vendor/pouchdb/pouchdb.relational-pouch.js');
+    // app.import('vendor/pouchdb/shims.js', {
+    //   exports: { 'pouchdb': [ 'default' ]}
+    // });
   }
 };
